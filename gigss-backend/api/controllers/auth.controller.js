197f2e2c43d1e95dac38d22b0ba72db6db0e3c4f
Rupesh Prasad <93862233+Rupesh1011@users.noreply.github.com>
2024-07-09 01:29:06 +0530

    Update auth.controller.js

--- gigss-backend/api/controllers/auth.controller.js
+++ gigss-backend/api/controllers/auth.controller.js
@@ -22,32 +22,33 @@ export const register = async (req,res,next) =>{
 
 };
 
-export const login =async (req,res,next) =>{
-    
+export const login = async (req, res, next) => {
     try {
-        const user = await User.findOne({username: req.body.username});
+        const user = await User.findOne({ username: req.body.username });
     
-        if(!user) return next(createError(404,"User not found!")); 
+        if (!user) return next(createError(404, "User not found!"));
 
-        const isCorrect =  bcrypt.compareSync(req.body.password, user.password)
-        if(!isCorrect) return  next(createError(400,"Wrong username or password!"));
+        const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+        if (!isCorrect) return next(createError(400, "Wrong username or password!"));
 
+        const { password, ...info } = user._doc;
 
-        const {password, ...info} = user._doc;
+        const token = jwt.sign(
+            { id: info._id, isSeller: info.isSeller },
+            process.env.JWT_KEY,
+            { expiresIn: "1h" }  // You can set an expiration time for better security
+        );
 
-        const token = jwt.sign({
-            id: info._id,
-            isSeller:info.isSeller,
-        },`${process.env.JWT_KEY}`); 
-        
-        res.cookie("accessToken", token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' }).status(200).send(info);     
-
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
+        }).status(200).send(info);
     } catch (err) {
         console.error(err);
-        next(err)
+        next(err);
     }
-
-}
+};
 
 export const logout = async(req,res) =>{
     res.clearCookie("accessToken",{
