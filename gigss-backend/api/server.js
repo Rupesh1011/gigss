4331c665277540845c691264d49d1a0c21869267
Rupesh Prasad <93862233+Rupesh1011@users.noreply.github.com>
2024-07-08 20:00:07 +0530

    Update server.js

--- gigss-backend/api/server.js
+++ gigss-backend/api/server.js
@@ -33,7 +33,7 @@ const connect = async() => {
   }
 };
 
-app.use(cors({origin:process.env.FRONTEND_URL,credentials:true}));
+app.use(cors({origin: ['https://gigss.vercel.app'],credentials:true}));
 app.use(express.json());
 app.use(cookieParser());
  
