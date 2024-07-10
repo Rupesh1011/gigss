3a7c2c5cc1f5d273a7ebb21308730e38aeee589d
Rupesh Prasad <93862233+Rupesh1011@users.noreply.github.com>
2024-07-10 07:09:14 +0530

    Update CheckoutForm.jsx

--- gigss-ui/src/components/checkoutForm/CheckoutForm.jsx
+++ gigss-ui/src/components/checkoutForm/CheckoutForm.jsx
@@ -69,7 +69,7 @@ const CheckoutForm = () => {
     console.error(result.error.message);
   } else if (result.paymentIntent.status === 'succeeded') {
     // Payment succeeded, handle success
-    window.location.href = "https://giggs.vercel.app/success"; // Redirect to success page
+    window.location.href = "https://gigss.vercel.app/success"; // Redirect to success page
   }
 })
 .catch(function(error) {
